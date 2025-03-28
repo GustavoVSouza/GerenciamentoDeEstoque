247ba30488d81cd16c918a3461491ab3471f6004
Gustavo Souza <97368708+GustavoVSouza@users.noreply.github.com>
2025-03-28 12:12:40 -0300

    CHORE: Alteração base

--- Backend/src/db.js
+++ Backend/src/db.js
@@ -3,8 +3,8 @@ const { Pool } = require('pg');
 const pool = new Pool({
   host: 'localhost',
   user: 'postgres', // ou o usuário que você configurou
-  password: 'Viruz@2003', // sua senha
-  database: 'synthex', // o nome do seu banco de dados PostgreSQL
+  password: 'Senha', // sua senha
+  database: 'nomeDoBanco', // o nome do seu banco de dados PostgreSQL
   port: 5432, // porta padrão do PostgreSQL
 });
 
